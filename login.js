7704598de0d8499752dea0afe003c3447db96f56
Angela Acquista <157067144+AngelaAcquista@users.noreply.github.com>
2026-03-26 01:00:44 -0400

    Update login.js

--- login.js
+++ login.js
@@ -78,6 +78,18 @@ function togglePassword(inputId = "password", toggleId = "togglePassword") {
     toggleText.textContent = "Show";
   }
 }
+function toggleConfirmPassword() {
+  const passwordInput = document.getElementById("confirm-password");
+  const toggleText = document.getElementById("toggleConfirm");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    toggleText.textContent = "Hide";
+  } else {
+    passwordInput.type = "password";
+    toggleText.textContent = "Show";
+  }
+}
 
 // ─── Event Handlers ───────────────────────────────────────────────────────────
  
